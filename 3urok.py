130725775483117d59d70fec72c05bc4fceb43a1
Navintas22 <iiemperor575@gmail.com>
2023-12-14 17:48:57 +0200

    122/12/2023

--- 3urok.py
+++ 3urok.py
@@ -27,6 +27,7 @@ class Human:
             print(f"За столь большую работу вы получаете аудиенцию с самим Си Цзиньпином!")
         self.money += money
         self.enjoyment -= 5
+        self.home.food -= 2
 
     def chill(self):
         print("Отдыхаем")
@@ -38,25 +39,50 @@ class Human:
         print("Вы пошли скупиться в магазин за продуктами и не только(партии всё-равно, мягко говоря)")
         money = random.randint(3, 10)
         enjoyment = random.randint(5, 15)
+        foodyha = random.randint(5, 10)
         print(f"Вы потратили {money} мексиканских песо, зато у вас теперь дома есть что покушать(наверное)")
         self.money -= money
         self.enjoyment += enjoyment
+        self.home.food += foodyha
 
     def clean_house(self):
-        print("Вы чистите дом(партия одобряет).")
-        musor = random.randint(3, 8)
-        enjoyment = random.randint(5, 10)
-        print(f"Вы почистили дом и убрали {musor}кг мусора!")
-        self.enjoyment -= enjoyment
+        if Home != None:
+            print("Дом купи")
+        else:
+            print("Вы чистите дом(партия одобряет).")
+            musor = random.randint(3, 8)
+            enjoyment = random.randint(5, 10)
+            cleanliness_level = random.randint(10, 15)
+            print(f"Вы почистили дом и убрали {musor}кг мусора!")
+            self.enjoyment -= enjoyment
+            self.home.cleanliness_level -= cleanliness_level
 
     def life(self):
-        pass
+        self.work()
+        r = random.randint(1, 4)
+        if r == 1:
+            self.work()
+        elif r == 2:
+            self.chill()
+        elif r == 3:
+            self.clean_house()
+        else:
+            self.shopping()
+
 
     def is_alive(self):
-        pass
+        if self.money <= 0 or self.home.food <= 0:
+            print("смэрть.")
+            return False
+        return True
 
     def info(self):
-        pass
+        print("—————————————————")
+        print(f"Стан персонажа {self.name}:")
+        print(f"====Рiвень задоволення====: {self.enjoyment}")
+        print(f"======Залишок грошей======: {self.money}")
+        print(f"======Кiлькiсть iжi=======: {self.home.food}")
+        print(f"====Порядок в кiмнатi=====: {self.home.cleanliness_level}")
 
 class Car:
     def __init__(self, marka):
@@ -77,14 +103,18 @@ class Car:
 
 class Home:
     def __init__(self):
-        self.food = 0
+        self.food = 50
         self.cleanliness_level = 50
 
-human1 = Human("ZELENSKY")
-human2 = Human("BAIDEN")
-human3 = Human("and many billiards rockets...")
-car = Car("Leopard")
-car.add_passenger(human1)
-car.add_passenger(human2)
-car.add_passenger(human3)
-car.passengers_info()
+human = Human("ZELENSKY", car=Car("Leopard"), home=Home())
+day = 1
+while(human.is_alive()):
+    print()
+    print(f"День {day}")
+    human.life()
+    human.info()
+    day += 1
+
+'''car = Car("LEOPARD")
+car.add_passenger(human)
+car.passengers_info()'''
